8f45d13b48de5cbd9784c629aa1ac33920d055e0
Paul Boiseau <paulboiseau@gmail.com>
2016-02-01 17:07:02 +0100

    setup projet with fabric

---
+++ Openclassroom/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Openclassroom
+//
+//  Created by Paul on 01/02/2016.
+//  Copyright © 2016 paulboiseau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
