e475dc34b2da9f85c9e51435097c5af5d0e87545
Paul Boiseau <paulboiseau@gmail.com>
2016-03-05 16:24:33 +0100

    add signIn and register view controller

--- Openclassroom/Controller/Sign/LoginViewController.swift
+++ Openclassroom/Controller/Sign/LoginViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        initializeView()
+    }
+    
+    func initializeView() {
+        loginButton.OCdefaultButton(UIColorFromRGBA("FFFFFFF"))
+    }
+    
+    @IBAction func cancelAction(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- Openclassroom/Controller/Sign/RegisterViewController.swift
+++ Openclassroom/Controller/Sign/RegisterViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initializeView()
+    }
+    
+    func initializeView() {
+        registerButton.OCdefaultButton(UIColorFromRGBA("FFFFFF"))
+    }
+    
+    @IBAction func cancelAction(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
