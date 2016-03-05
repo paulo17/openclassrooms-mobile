1bae1dbe9543091aa286479a42101daba080b279
Paul Boiseau <paulboiseau@gmail.com>
2016-03-05 13:12:39 +0100

    integration register view

--- Openclassroom/AppDelegate.swift
+++ Openclassroom/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        Fabric.with([Crashlytics.self])
+        Fabric.with([Crashlytics.self, Answers.self])
         
         presentWalkthrough()
         
