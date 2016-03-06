c49b4b658e8e3cc34e934a8427f88b0b9ebf6cb5
Paul Boiseau <paulboiseau@gmail.com>
2016-03-06 17:05:01 +0100

    add calendar and user icon

--- Openclassroom/Controller/MainViewController.swift
+++ Openclassroom/Controller/MainViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var tasksCollectionView: UICollectionView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private let cellIdentifier = "leconCell"
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        initializeView()
+    }
+    
+    func initializeView() {
+        navigationBar.OCDefaultNavigationBar()
+        startButton.OCdefaultButton(UIColor.whiteColor())
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        return cell
     }
     
 }
