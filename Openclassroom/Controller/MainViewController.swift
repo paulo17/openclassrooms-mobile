0c5dbc12579411b0cecad6fff6945b66c9911524
Paul Boiseau <paulboiseau@gmail.com>
2016-03-09 11:12:22 +0100

    add current page control index

--- Openclassroom/Controller/MainViewController.swift
+++ Openclassroom/Controller/MainViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
     
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var tasksCollectionView: UICollectionView!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     private let cellIdentifier = "leconCell"
     
@@ -25,19 +26,27 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         startButton.OCdefaultButton(UIColor.whiteColor())
     }
     
+    // MARK: - Collection view
+    
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        pageControl.numberOfPages = 4 // set page control number to the number of element in collection view
         return 4
     }
     
-    
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        pageControl.currentPage = indexPath.row // set current page control to current item index
+        
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! LeconCollectionViewCell
         
         return cell
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        
     }
     
 }
