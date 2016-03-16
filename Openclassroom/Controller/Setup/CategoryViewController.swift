8801708a2d1d9d257d19e2cfb6484e1e7e3852c8
Paul Boiseau <paulboiseau@gmail.com>
2016-03-16 15:18:58 +0100

    add navigation controller for setup

---
+++ Openclassroom/Controller/Setup/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewController.swift
+//  Openclassroom
+//
+//  Created by Paul on 16/03/2016.
+//  Copyright © 2016 paulboiseau. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+}
