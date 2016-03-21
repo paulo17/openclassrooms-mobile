db67ee59320fe4a94438cbea2dc7d3c1be3185ed
Paul Boiseau <paulboiseau@gmail.com>
2016-03-21 17:30:10 +0100

    setup for tableview

--- Openclassroom/Controller/Setup/ObjectiveViewController.swift
+++ Openclassroom/Controller/Setup/ObjectiveViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ObjectiveViewController: UIViewController {
+class ObjectiveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var categoryName: UILabel!
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryObjectiveNumber: UILabel!
+    @IBOutlet weak var objectiveTableView: UITableView!
+    
+    let objectiveCellIdentifier = "objectiveCell"
     
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
+        objectiveTableView.delegate = self
+        objectiveTableView.dataSource = self
+    }
+    
+    // MARK: - TableView
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = objectiveTableView.dequeueReusableCellWithIdentifier(objectiveCellIdentifier, forIndexPath: indexPath) as! ObjectiveCell
         
+        return cell
     }
 }
---
+++ Openclassroom/View/Cell/ObjectiveCell.swift
@@ -0,0 +1,13 @@
+//
+//  ObjectiveCell.swift
+//  Openclassroom
+//
+//  Created by Paul on 21/03/2016.
+//  Copyright © 2016 paulboiseau. All rights reserved.
+//
+
+import UIKit
+
+class ObjectiveCell: UITableViewCell {
+    
+}
