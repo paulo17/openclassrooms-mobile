afd0eeee6bb810d977dcbbbf88c0db8b1cb7c4eb
Paul Boiseau <paulboiseau@gmail.com>
2016-03-23 21:16:35 +0100

    add/remove day from selected days

--- Openclassroom/Controller/Setup/DayViewController.swift
+++ Openclassroom/Controller/Setup/DayViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class DayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+protocol DaySwitchDelegate: class {
+    func didChangeSwitchState(sender: DayCell, status: Bool)
+}
+
+class DayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, DaySwitchDelegate {
     
     @IBOutlet weak var timeTableView: UITableView!
     @IBOutlet weak var validateButton: UIButton!
     
     let timeCellIdentifier = "timeCell"
+    let daysOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+    
+    lazy var selectedDays = [Int: String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +38,24 @@ class DayViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     // MARK: - TableView
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return daysOfWeek.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = timeTableView.dequeueReusableCellWithIdentifier(self.timeCellIdentifier, forIndexPath: indexPath) as! DayCell
+        cell.initializeCellWithContent(daysOfWeek[indexPath.row], atIndex: indexPath.row)
+        cell.delegate = self // delegate for switch change event
         
         return cell
+    }
+    
+    func didChangeSwitchState(sender: DayCell, status: Bool) {
+        if let day = sender.dayLabel.text {
+            if status {
+                selectedDays.updateValue(day, forKey: sender.index)
+            } else {
+                selectedDays.removeValueForKey(sender.index)
+            }
+        }
     }
 }
--- Openclassroom/View/Cell/DayCell.swift
+++ Openclassroom/View/Cell/DayCell.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class DayCell: UITableViewCell {
 
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var daySwitch: UISwitch!
+    
+    weak var delegate: DaySwitchDelegate?
+    var index: Int!
+    
+    func initializeCellWithContent(day: String, atIndex index: Int) {
+        dayLabel.text = day
+        self.index = index
+    }
+    
+    @IBAction func switchChangedAction(sender: UISwitch) {
+        delegate?.didChangeSwitchState(self, status: sender.on)
+    }
 }
