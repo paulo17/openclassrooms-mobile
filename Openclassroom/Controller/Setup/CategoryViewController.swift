30c4ba857905114e49e6dafedea3242d058281b2
Paul Boiseau <paulboiseau@gmail.com>
2016-03-30 11:43:36 +0200

    remove validate button for category

--- Openclassroom/Controller/Setup/CategoryViewController.swift
+++ Openclassroom/Controller/Setup/CategoryViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class CategoryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var validateButton: UIButton!
     @IBOutlet weak var categoryCollectionView: UICollectionView!
     
     let categoryCellIdentifier = "categoryCell"
@@ -22,8 +21,6 @@ class CategoryViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         categoryCollectionView.delegate = self
         categoryCollectionView.dataSource = self
-        
-        validateButton.OCdefaultButton(UIColorFromRGBA("ceced2"))
         
         self.categories = getStaticCategory()
     }
@@ -51,44 +48,18 @@ class CategoryViewController: UIViewController, UICollectionViewDelegate, UIColl
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(categoryCellIdentifier, forIndexPath: indexPath) as! CategoryCell
         
-        let category = categories[indexPath.row]
-        
-        if let selected = selectedCategory {
-            cell.initializeCellWithContent(current: category, selected: selected)
-        } else {
-            cell.initializeCellWithContent(category)
-        }
-        
+        cell.initializeCellWithContent(categories[indexPath.row])
+    
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let selectedCell = collectionView.cellForItemAtIndexPath(indexPath) as! CategoryCell
         selectedCell.categoryImage.alpha = 1.0
-        
-        validateButton.OCdefaultButton(UIColorFromRGBA("F39539"))
         selectedCategory = categories[indexPath.row]
     }
     
-    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
-        let selectedCell = collectionView.cellForItemAtIndexPath(indexPath) as! CategoryCell
-        selectedCell.categoryImage.alpha = 0.5
-        
-        validateButton.OCdefaultButton(UIColor.OCSilverTwoColor())
-        selectedCategory = nil
-    }
-    
     // MARK: - Navigation
-    
-    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
-        if identifier == "categoryToObjective" {
-            guard (selectedCategory != nil) else {
-                return false
-            }
-        }
-        
-        return true
-    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "categoryToObjective" {
